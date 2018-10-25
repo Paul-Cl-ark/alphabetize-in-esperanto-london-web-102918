6a92642a22e767e5e8ec61c7ac058a01b2261263
Paul Clark <clark.paul123@gmail.com>
2018-10-25 21:46:44 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,8 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 # use sort_by |phrase| phrase first letter alphabetically
 
+
+# does not work because of dodgy characters
 def alphabetize(arr)
   arr.sort_by do |phrase| phrase.downcase
   end
