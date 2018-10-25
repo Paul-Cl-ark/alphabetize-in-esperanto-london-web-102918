ea2b79fa2c8f3be1b3b9664e16b3236cddc9ca4d
Paul Clark <clark.paul123@gmail.com>
2018-10-25 21:47:50 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,11 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 
 # does not work because of dodgy characters
+# def alphabetize(arr)
+#  arr.sort_by do |phrase| phrase.downcase
+#  end
+# end
+
 def alphabetize(arr)
   arr.sort_by do |phrase| phrase.downcase
   end
